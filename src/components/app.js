84ca9e8e927d880b9e5866373e1346d3b846d38e
Nathan Cooper <nac33@students.uwf.edu>
2018-04-22 00:52:17 -0500

    Added stubs for the home, project, and about page and setup react router

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+
+import Projects from './projects';
+import About from './about';
+import Home from './home';
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        Hello, React!
-      </div>
+      <HashRouter>
+        <div>
+          <Switch>
+            <Route path = "/projects" component = { Projects } />
+            <Route path = "/about" component = { About } />
+            <Route path = "/" component = { Home } />
+          </Switch>
+        </div>
+      </HashRouter>
     );
   }
 }
