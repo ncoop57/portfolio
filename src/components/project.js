fc90eb520899b6f7395cd1a426249df858338f61
Nathan Cooper <nac33@students.uwf.edu>
2018-04-30 22:08:03 -0500

    Fix title of cards to always be title case

--- src/components/project.js
+++ src/components/project.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import { Card, CardImg, CardBody, CardTitle,
   CardSubtitle, Button } from 'reactstrap';
@@ -6,11 +7,15 @@ const Project = (props) => {
   const { name, full_name, url } = props;
   const icon_src = `https://raw.githubusercontent.com/${full_name}/master/icon.png`;
   return (
-    <Card>
-      <CardImg top width = "100%" src = { icon_src } onError = { (e) => { e.target.src = "images/default.png" } } alt = "Icon Not Provided" />
+    <Card className = "project shadow p-3">
+      <CardImg
+        top width = "100%"
+        src = { icon_src }
+        onError = { (e) => { e.target.src = "images/default.png" } }
+        alt = "Project Icon" />
       <CardBody>
-        <CardTitle>{ name }</CardTitle>
-        <Button color = "primary" href = { url }>View Repo</Button>
+        <CardTitle>{ _.startCase(_.camelCase(name)) }</CardTitle>
+        <Button className = "shadow" color = "primary" href = { url }>View Repo</Button>
       </CardBody>
     </Card>
   );
