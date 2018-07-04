a2039deeb37ac8fcd5a3dfb84c82c15198ebcb15
Nathan Cooper <nac33@students.uwf.edu>
2018-07-04 17:39:29 -0500

    Fix some grammar and spelling mistakes and format the look to be easier on the eyes

--- src/components/about.js
+++ src/components/about.js
@@ -1,25 +1,13 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import classNames from "classnames";
 import { withStyles } from "material-ui/styles";
-import {
-  Avatar,
-  Card,
-  CardContent,
-  CardMedia,
-  Paper,
-  Typography
-} from "material-ui";
+import { CardMedia, Divider, Paper, Typography } from "material-ui";
 
 const styles = theme => ({
   root: {
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 2,
     paddingBottom: theme.spacing.unit * 2
-  },
-  resume: {
-    // width: "100%",
-    // height: 600
   },
   avatar: {
     margin: 10
@@ -44,27 +32,31 @@ const About = props => {
         >
           <img src="./images/profile.jpg" alt="Nathan Cooper" width="75%" />
         </CardMedia>
-        <Typography variant="headline" component="h3">
+        <Typography className="text-center" variant="headline" component="h3">
           I'm a nerd.
         </Typography>
+        <Divider />
+        <br />
         <Typography component="p">
           Hi there, my name is Nathan Cooper. I am currently a PhD student at
           the College of William and Mary, studying Software Engineering. I
           always loved playing video games with friends and on my own, so, when
           I learned I could make them myself through programming a computer to
-          do it, I fell in love with programming. I wrote my first program with
+          do it, I fell in love with programming. I wrote my first program when
           I was 12 years old, it was a program to mod a game I was addicted to
           at the time called Roblox. From there I went on to mod other games,
           like MineCraft, and "attempt" to make my own. I eventually decided to
           make this passion of mine a career, so, I went for a Bachelor's degree
-          in Software Engineering from the University of West Florida. Though, I
-          switched my career path from games to now education, I still love
-          playing video games with friends to pass the time. As a hobby I work
-          on personal projects, of which you can find a list of, hopefully
-          mostly complete, ones on my projects page. Below you will find my
-          resume. If you have any questions or just want to say hi, feel free to
-          use my fully custom made Contact system which you can access using the
-          sidebar!
+          in Software Engineering from the University of West Florida and
+          graduated from there in Spring of 2018. Though, I switched my career
+          path from games to now education using Software Engineering, I still
+          love playing video games with friends to pass the time. As a hobby, I
+          work on personal projects which you can find a list of, hopefully
+          mostly complete, ones on my Projects page. Below you will find my
+          resume if you are curious in any of my other experiences or
+          accomplishments. If you have any questions or just want to say hi,
+          feel free to use my fully custom made Contact system that you can
+          access using the sidebar!
         </Typography>
 
         <br />
